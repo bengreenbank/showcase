6bfcf9dfa11c5c907b6cf173de783ad011177052
Ben Greenbank <ben@destinationcore.com>
2022-03-24 18:57:25 +0000

    Created button component and added GitHub link to header

---
+++ components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink
+    v-if="external === false"
+    :to="url"
+    :class="classes">
+    {{ text }}
+  </NuxtLink>
+
+  <a
+    v-else
+    :href="url"
+    :class="classes"
+    target="_blank">
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  data() {
+    return {
+      classes: 'bg-gradient-to-r from-green-400 to-emerald-400 text-slate-800 px-4 py-2.5 rounded-md font-semibold transition-shadow'
+    }
+  },
+  props: {
+    external: {
+      type: Boolean,
+      default: false
+    },
+    url: {
+      type: String,
+      default: '/'
+    },
+    text: {
+      type: String
+    }
+  }
+}
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <header class="w-full p-6 bg-slate-900">
-    <div class="container flex justify-between">
+  <header class="w-full py-8 bg-slate-900">
+    <div class="container flex items-center justify-between">
       <NuxtLink to="/">
         <img src="~/assets/images/logo.svg" alt="greenbank.dev" width="200" height="27">
       </NuxtLink>
+
+      <Button :external="true" url="https://github.com/bengreenbank" text="View my GitHub" />
     </div>
   </header>
 </template>
