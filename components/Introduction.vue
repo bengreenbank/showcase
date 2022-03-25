6afde57efaf480a04686399dd0e594671950f4bf
Ben Greenbank <ben@destinationcore.com>
2022-03-25 21:26:49 +0000

    Created Introduction.vue component for index page

---
+++ components/Introduction.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="bg-purple text-white flex-grow flex items-center">
+    <section class="container pr-20">
+      <h1 class="text-5xl lg:text-8xl font-extrabold tracking-wide mb-8">
+        Hi there 👋
+      </h1>
+
+      <p class="text-2xl lg:text-4xl tracking-wide !leading-relaxed mb-6">
+        I'm Ben, a <u>frontend developer</u> from Suffolk (the one in the United Kingdom). I have a passion for writing <u>clean</u>, <u>performant</u> and <u>modular</u> code.
+
+        <br class="mb-8"/>
+
+        My key technologies are <u>CraftCMS</u>, <u>Twig</u>, <u>Vue</u> and <u>TailwindCSS</u>. I love exploring new technologies, and my tech stack is always evolving.
+      </p>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Introduction"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <Introduction/>
 </template>
 
 <script>
