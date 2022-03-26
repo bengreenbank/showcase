61f8cb5ed8f1e9ea63719a9d52aae27e40efc3c4
Ben Greenbank <ben@destinationcore.com>
2022-03-26 08:42:22 +0000

    Updated title tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'showcase',
+    title: 'Ben Greenbank, Frontend Web Developer',
     htmlAttrs: {
       lang: 'en'
     },
