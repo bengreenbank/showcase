5b6109d03f21df2e3662584688895f6b9c3eb026
Ben Greenbank <ben@destinationcore.com>
2022-04-25 11:09:39 +0100

    Added navigation items to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,38 @@
 <template>
-  <header class="w-full bg-purple py-10 shadow-lg">
-    <div class="container flex items-center justify-between">
+  <header class="flex w-full justify-between bg-purple py-10 shadow-lg">
+    <nav class="container flex items-center justify-between">
       <img
         src="~/assets/images/logo.svg"
         alt="greenbank.dev"
         width="200"
         height="27"
       />
-    </div>
+
+      <ul class="text-xl text-white font-semibold italic flex gap-6">
+        <li v-for="link in links">
+          <NuxtLink :class="{'text-pink': link.url === $route.path}" :to="link.url"> {{ link.title }} </NuxtLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
+  data() {
+    return {
+      links: {
+        home: {
+          title: 'Home',
+          url: '/'
+        },
+        projects: {
+          title: 'Projects',
+          url: '/projects'
+        }
+      }
+    }
+  }
 };
 </script>
-
-<style scoped></style>
