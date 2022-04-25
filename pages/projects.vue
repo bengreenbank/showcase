7de89186e7c6c4dc002d91fabdc297e9bef9f723
Ben Greenbank <ben@destinationcore.com>
2022-04-25 14:07:01 +0100

    Added placeholder on projects.vue page

---
+++ pages/projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="container flex items-center justify-center text-3xl font-semibold italic text-pink"
+  >
+    <h1>Coming Soon...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+};
+</script>
